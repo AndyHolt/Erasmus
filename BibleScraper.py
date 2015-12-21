7a1104eb2c851e6d08ecf870393bd4651ff266b3
Andy Holt <andrew.holt@hotmail.co.uk>
2015-12-21 22:52:29 +0000

    Multiple versions support added.

--- BibleScraper.py
+++ BibleScraper.py
@@ -9,8 +9,15 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-passage = 'Galatians 3:1-4:7'
-version = 'ESVUK'
+passage = 'Galatians 3:1-9'
+version = 'ESVUK;NIVUK;SBLGNT'
+
+versions_list = re.split(r';', version)
+passage_name = re.sub(r'([1-3]*\s*[A-Za-z]{1,3})[A-Za-z]*\s*',
+                      r'\1',
+                      passage, flags=re.UNICODE)
+passage_name = re.sub(r':', r'_', passage_name, flags=re.UNICODE)
+passage_name = re.sub(r'\s', r'', passage_name, flags=re.UNICODE)
 
 # get page html code from biblegateway.com
 options = {'search': passage, 'version': version}
@@ -19,52 +26,53 @@ r = requests.get(url, options)
 
 # parse html and extract the bible text
 page_html = BeautifulSoup(r.text, "html.parser")
-passage_html_long = page_html.find(class_="passage-text")
-passage_html = passage_html_long.find_all("p")
+translations_long_passage_list = page_html.find_all(class_="passage-text")
 
-# prepare list for paragraph texts
-paragraph_text = []
+loopcount = 0
+for translation in translations_long_passage_list:
+    passage = translation.find_all("p")
 
-# for each paragrah (except last one, which is just copyright statement),
-# reformat
-for paragraph in passage_html[:-1]:
-    # remove crossreferences
-    # [todo] - add crossreferences as optional paratext element
-    [c.decompose() for c in paragraph.find_all("sup", class_="crossreference")]
+    # prepare list for paragraph texts
+    paragraph_text = []
 
-    # remove footnotes
-    # [todo] - add footnotes as optional paratext element
-    [f.decompose() for f in paragraph.find_all("sup", class_="footnote")]
+    # for each paragrah (except last one, which is just copyright statement),
+    # reformat
+    for paragraph in passage[:-1]:
+        # remove crossreferences
+        # [todo] - add crossreferences as optional paratext element
+        [c.decompose() for c in paragraph.find_all("sup", class_="crossreference")]
 
-    # latexify chapter numbers
-    for c in paragraph.find_all(class_="chapternum"):
-        chap_no = c.string
-        ltx_chap_no = re.sub(r'([0-9]+)\s',
-                             r'\\cn{\1}',
-                             chap_no,
-                             flags=re.UNICODE)
-        c.string = ltx_chap_no
+        # remove footnotes
+        # [todo] - add footnotes as optional paratext element
+        [f.decompose() for f in paragraph.find_all("sup", class_="footnote")]
 
-    # latexify verse numbers
-    for v in paragraph.find_all(class_="versenum"):
-        verse_no = v.string
-        ltx_verse_no = re.sub(r'([0-9]+)\s',
-                              r'\\vn{\1}',
-                              verse_no,
-                              flags=re.UNICODE)
-        v.string = ltx_verse_no
+        # latexify chapter numbers
+        for c in paragraph.find_all(class_="chapternum"):
+            chap_no = c.string
+            ltx_chap_no = re.sub(r'([0-9]+)\s',
+                                 r'\\cn{\1}',
+                                 chap_no,
+                                 flags=re.UNICODE)
+            c.string = ltx_chap_no
 
-    # extract text from html
-    paragraph_text.append(paragraph.get_text())
+        # latexify verse numbers
+        for v in paragraph.find_all(class_="versenum"):
+            verse_no = v.string
+            ltx_verse_no = re.sub(r'([0-9]+)\s',
+                                  r'\\vn{\1}',
+                                  verse_no,
+                                  flags=re.UNICODE)
+            v.string = ltx_verse_no
 
+        # extract text from html
+        paragraph_text.append(paragraph.get_text())
 
-# save to file
-# f = open('gal3_16esvuk.html', 'w')
-# f.write(passage_text.encode('utf8'))
-# f.close
-with open('gal3_16esvuk.html', 'a') as f:
-    for paragraph in paragraph_text[:-1]:
-        f.write(paragraph.encode('utf8'))
-        f.write('\n')
-        f.write('\n')
-    f.write(paragraph_text[-1].encode('utf8'))
+    # save to file
+    filename = passage_name + versions_list[loopcount] + '.txt'
+    loopcount += 1
+    with open(filename, 'a') as f:
+        for paragraph in paragraph_text[:-1]:
+            f.write(paragraph.encode('utf8'))
+            f.write('\n')
+            f.write('\n')
+        f.write(paragraph_text[-1].encode('utf8'))
